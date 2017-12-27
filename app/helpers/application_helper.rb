c2e2cffdb26ed18c71dd4b2ce97d1a8a65910583
Arnaud Levy <arnaud@semiodesign.com>
2017-12-27 10:41:28 +0100

    menu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def active_on(path)
+    raw ' class="active"' if current_page? path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'demo/edit' => 'demo#edit', as: :demo_edit
+  get 'demo/show' => 'demo#show', as: :demo_show
+  get 'demo/localize' => 'demo#localize', as: :demo_localize
+  get 'demo/show-localized' => 'demo#show_localized', as: :demo_show_localized
+  get 'demo/data' => 'demo#data', as: :demo_data
   root to: 'application#index'
 end
