4dcb8bfa12170838efc67e45410b1527b4f8e4a5
Arnaud Levy <alevy@lespoupees.paris>
2018-04-23 14:59:59 +0200

    fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,8 +1,8 @@
 //= require jquery
 //= require jquery-ui
 //= require bootstrap
+//= require summernote
 //= require angular
 //= require angular-animate
-//= require summernote
 //= require angular-summernote
 //= require ui-sortable
