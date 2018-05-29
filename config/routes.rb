02e9819ff929ec3dc04a1e15e2151e48c5309346
Arnaud Levy <alevy@lespoupees.paris>
2018-05-29 16:49:09 +0200

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  promethee
   resources :pages do
     resources :localizations
   end
   get 'demo/edit' => 'demo#edit', as: :demo_edit
+  post 'demo/edit' => 'demo#index'
   get 'demo/show' => 'demo#show', as: :demo_show
   get 'demo/localize' => 'demo#localize', as: :demo_localize
   get 'demo/show-localized' => 'demo#show_localized', as: :demo_show_localized
